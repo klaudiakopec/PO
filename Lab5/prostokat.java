c6d7d2845c0eae40e43e701a813376526b9a4563
klaudiakopec <92815040+klaudiakopec@users.noreply.github.com>
2022-04-24 23:39:54 +0200

    Create prostokat.java

---
+++ Lab5/prostokat.java
@@ -0,0 +1,27 @@
+public prostokat(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void Info(){
+        System.out.println("Pole: "+(a*b)+" \nObwod: "+(a+a+b+b)+" \nPrzekatna "+przekatna);
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+
+
